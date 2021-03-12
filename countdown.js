3d705b79500250bdd38104a1cfa52e42d058422a
david <omotayodavid9@gmail.com>
2021-03-12 14:05:40 -0800

    first commit

---
+++ countdown.js
@@ -0,0 +1,26 @@
+const countDownDate = new Date("Feb 29, 2023 00:59:59").getTime();
+
+const interv = setInterval(() => {
+    
+    const now = new Date().getTime();
+
+    const distance = countDownDate - now;
+
+    const days = Math.floor(distance /(1000 * 60 * 60 * 24));
+
+    const hours = Math.floor(distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+
+    const minutes = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
+
+    const seconds = Math.floor(distance % (1000 * 60) / 1000);
+
+    const paragraph = document.querySelector(".countdown");
+    paragraph.textContent = `${days} d : ${hours} h : ${minutes} m : ${seconds} s`;
+ 
+    if (distance <= 0) {
+        clearInterval(interv);
+
+     paragraph.textContent = "Time to kick that bastard out!"
+    }
+
+}, 1000);
